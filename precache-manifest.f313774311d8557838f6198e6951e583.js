ee3540194f853751d89a001b3358df9bd7eb74cd
Hemant Wagh <hemantwagh07@gmail.com>
2020-09-23 17:33:58 +0530

    Updates

---
+++ precache-manifest.f313774311d8557838f6198e6951e583.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdc8a983d26378f6d87cf486af77f2e5",
+    "url": "/react-robot-rolodex/index.html"
+  },
+  {
+    "revision": "3e637bf2e6a65c12f59f",
+    "url": "/react-robot-rolodex/static/css/main.52654f7c.chunk.css"
+  },
+  {
+    "revision": "03cfb61c7bd5c058edf0",
+    "url": "/react-robot-rolodex/static/js/2.806642a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-robot-rolodex/static/js/2.806642a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e637bf2e6a65c12f59f",
+    "url": "/react-robot-rolodex/static/js/main.044f2622.chunk.js"
+  },
+  {
+    "revision": "4f1bfc35b64e14d2fe68",
+    "url": "/react-robot-rolodex/static/js/runtime-main.2f3686b5.js"
+  }
+]);
